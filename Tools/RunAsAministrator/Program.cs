0874931f5459173da664e76f70770a8b6efc91bc
qizhenlu <q@qizl.cn>
2019-01-18 11:56:16 +0800

    添加调用管理员权限运行命令的工具

---
+++ Tools/RunAsAministrator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+using System.Linq;
+
+namespace EnjoyCodes.RunAsAministrator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args?.Length > 0)
+                Process.Start(args.First(), string.Join(" ", args.Skip(1)));
+        }
+    }
+}
